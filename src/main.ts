c557334a4aded7849e05a4c21b64f84dfe641d1f
reynoldputra <reynoldputra1@gmail.com>
2023-08-19 21:16:08 +0700

    feat: backend port env

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  await app.listen(process.env.BACKEND_PORT);
 }
 bootstrap();
