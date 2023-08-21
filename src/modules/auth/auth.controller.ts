3e7bcc9324330211ea0d602bd04adafc55fb9dc0
Rencist <dafianakhdan@gmail.com>
2023-08-22 04:56:39 +0700

    feat: api response and custom exception

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,50 +1,44 @@
-import { AuthService } from './auth.service'
-import { Controller, Post, Body, Req, Res } from '@nestjs/common'
-import { UserLoginDto } from './dtos/user-login.dto'
-import { Request, Response } from 'express'
-import { UserRegisterDto } from './dtos/user-register.dto'
+import { AuthService } from './auth.service';
+import {
+  Controller,
+  Post,
+  Body,
+  Req,
+  Res,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { UserLoginDto } from './dtos/user-login.dto';
+import { Request, Response } from 'express';
+import { UserRegisterDto } from './dtos/user-register.dto';
+import { CustomException } from '@/response/CustomException';
+import { sendSuccess } from '@/response/ApiResponse';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(
-    @Req() _: Request,
-    @Res() res: Response,
-    @Body() loginDto: UserLoginDto,
-  ): Promise<any> {
+  async login(@Req() _: Request, @Body() loginDto: UserLoginDto): Promise<any> {
     try {
-      const login = await this.authService.login(loginDto)
-      return res.status(200).json({
-        message: 'Login success',
-        data: login,
-      })
+      const login = await this.authService.login(loginDto);
+      return sendSuccess('Login success', 200, login);
     } catch (error) {
-      return res.status(404).json({
-        message: 'Login failed',
-        error: error.message,
-      })
+      if (error.status) throw new CustomException(error.message, error.status);
+      else throw new InternalServerErrorException(error);
     }
   }
 
   @Post('register')
   async register(
     @Req() _: Request,
-    @Res() res: Response,
     @Body() registerDto: UserRegisterDto,
   ): Promise<any> {
     try {
-      const register = await this.authService.register(registerDto)
-      return res.status(200).json({
-        message: 'Register success',
-        data: register,
-      })
+      const register = await this.authService.register(registerDto);
+      return sendSuccess('Register success', 200, register);
     } catch (error) {
-      return res.status(404).json({
-        message: 'Register failed',
-        error: error.message,
-      })
+      if (error.status) throw new CustomException(error.message, error.status);
+      else throw new InternalServerErrorException(error);
     }
   }
 }
